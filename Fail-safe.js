725e976aad892a53c42f62c35c7659f47b2c3211
DJ_Dave <84036151+algorave-dave@users.noreply.github.com>
2026-06-12 10:41:53 -0700

    Update Fail-safe.js

--- Fail-safe.js
+++ Fail-safe.js
@@ -8,7 +8,7 @@
 ▐▌   ▐▌ ▐▌▗▄█▄▖▐▙▄▄▖    ▗▄▄▞▘▐▌ ▐▌▐▌   ▐▙▄▄▖
 
 
-  HI! Welcome to my first "open sourced" project :) I've been waiting for the right moment to share some actual source code with you guys and after seeing the response to Fail-safe I decided now was the perfect time. Feel free to edit, remix, or just play around with this track, and if you make something you like, please share it in the discord [https://discord.gg/ZU6SUs6Mem] !!
+  HI! Welcome to my first "open source" project :) I've been waiting for the right moment to share some actual source code with you guys and after seeing the response to Fail-safe I decided now was the perfect time. Feel free to edit, remix, or just play around with this track, and if you make something you like, please share it in the discord [https://discord.gg/ZU6SUs6Mem] !!
   
   Make sure you download all the Fail-safe samples from my GitHub and import them into Strudel so you can hear the full track (menu => sounds => import sounds). You can just drag the whole main samples folder in and they will be linked correctly.
 
